0aefed8bfde2480e6a38236059ff50b98a785baf
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-16 05:23:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash [:yukihiro_matsumoto] = 
+	programmer_hash [:yukihiro_matsumoto] =
 	   {:known_for => "Ruby"},
 		 {:languages => ["LISP, C"]}
 
@@ -127,5 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages]= "Assembly"
+return programmer_hash
 end
