803a494c654c9101aa74d29b3b2bcdf2619c17b8
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-16 04:58:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
 	programmer_hash =
  		{
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
